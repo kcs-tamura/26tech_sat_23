07549a6308cfd46a1f442c5715b31e400f154162
kuro <kuro@legion-neko.com>
2026-05-23 13:36:41 +0900

    App.tsx 修正.

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,17 +1,7 @@
-export default function App(){
+export default function App() {
   return (
-    <>
-      <div>
-        <h1>お昼ご飯なう。</h1>
-      </div>
-      <br></br>
-      <div>
-        <h3>とりあえずポートフォリオデータ作成は 14:00 まで取ります。</h3>
-      </div>
-      <br></br>
-      <div>
-        <h3>Teamsのカメラつけてねー</h3>
-      </div>
-    </>
-  )
+    <div>
+      <h1>My Portfolio</h1>
+    </div>
+  );
 }
